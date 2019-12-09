3a44cfcb89b9f9db02b98f7c9039b81b0e9b4d91
Jacky Feng <jackyfeng530@yahoo.com>
2019-12-09 16:02:15 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -3,7 +3,7 @@ export function manageFriends(state, action){
     case 'ADD_FRIEND' :
       return {friends: [...state.friends, action.friend]}
     case 'REMOVE_FRIEND' :
-      return {friends: state.friends.filter(friend => friend.id !== action.id)}
+      return {friends: [state.friends.filter(friend => friend.id !== action.id)]}
     default: {friends: [ ]}
     }
   }
